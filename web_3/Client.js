d6e586bcfff3cc0a75205a4abd6e90fe1690c1b3
Ilya Kazakou <pouutewqer@gmail.com>
2021-02-10 22:39:19 +0300

    Finished 3th lab

--- web_3/Client.js
+++ web_3/Client.js
@@ -13,16 +13,7 @@ function useFirstCallback(){
 
         if(status === 404)
             return;
-        const messageBox = document.getElementById(idBox);
-
-        const img = document.createElement('img');
-        img.src = data.avatar_url;
-
-        const span = document.createElement("span");
-        span.innerHTML = data.bio + '<br>' + data.name;
-
-        messageBox.append(img);
-        messageBox.append(span);
+       writeMessages(idBox,data);
     })
 }
 
@@ -31,6 +22,9 @@ function FirstCallback(login, callback)
     let xhr = new XMLHttpRequest();
     xhr.callback = callback;
     xhr.open('GET',url + login);
+    xhr.onerror = ()=>{
+        alert('Wrong password')
+    }
     xhr.send();
 
     xhr.onload = () => callback('firstResult', JSON.parse(xhr.response),xhr.status);
@@ -39,7 +33,36 @@ function FirstCallback(login, callback)
 
 //Promise
 function useSecondCallback(){
-    
+    document.getElementById('secondResult').innerHTML = "";
+
+    const login = document.getElementById('login').value;
+    if(login === '' || login === null)
+        return;
+
+        fetch(url + login)
+            .then(response => response.json())
+            .then(userData => new Promise((resolve, reject) => {
+                if (userData !== null) {
+                    writeMessages('secondResult', userData);
+                    resolve();
+                }
+                reject(new Error("Wrong login"))
+            }))
+            .catch(err => alert(err))
+
+
 }
 
+function writeMessages(idBox, data)
+{
+    const messageBox = document.getElementById(idBox);
 
+    const img = document.createElement('img');
+    img.src = data.avatar_url;
+
+    const span = document.createElement("span");
+    span.innerHTML = data.bio + '<br>' + data.name;
+
+    messageBox.append(img);
+    messageBox.append(span);
+}
